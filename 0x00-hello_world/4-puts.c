296a755bbb1745042ba209f8440b79ad7145e903
Oluwakemi-Gs <estherkemit@gmail.com>
2023-09-09 06:55:22 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - this is the main put function
+ * Return: always 0
  */
 int main(void)
 {
-    return (0);
+	puts("\" Programming is like building a multilingual puzzle");
+	return (0);
 }
